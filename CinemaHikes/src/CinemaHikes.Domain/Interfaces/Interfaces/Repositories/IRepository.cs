aafb8639447e766f76e30c23286c9dd55be86862
andrvol <v0l0b.and0716@gmail.com>
2026-09-13 22:51:43 +0300

    Added basic repository interface(general CRUD interface)

---
+++ CinemaHikes/src/CinemaHikes.Domain/Interfaces/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+using CinemaHikes.Domain.Specification;
+
+namespace CinemaHikes.Domain.Interfaces.Interfaces.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken);
+
+    Task<List<T>> ListAsync(ISpecification<T> spec, CancellationToken cancellationToken);
+
+    Task<int> CountAsync(ISpecification<T> spec, CancellationToken cancellationToken);
+
+    Task AddAsync(T entity, CancellationToken cancellationToken);
+
+    void Update(T entity);
+
+    void Remove(T entity);
+}
---
+++ CinemaHikes/src/CinemaHikes.Domain/Specification/ISpecification.cs
@@ -0,0 +1,5 @@
+namespace CinemaHikes.Domain.Specification;
+
+public interface ISpecification<T>
+{
+}
