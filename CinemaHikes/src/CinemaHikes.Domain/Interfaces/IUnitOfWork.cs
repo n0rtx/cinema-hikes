b012d1b05685295789043787fda8e6803ad70a98
andrvol <v0l0b.and0716@gmail.com>
2026-09-14 12:56:32 +0300

    Added UoW pattern

--- CinemaHikes/src/CinemaHikes.Domain/Interfaces/IUnitOfWork.cs
+++ CinemaHikes/src/CinemaHikes.Domain/Interfaces/IUnitOfWork.cs
@@ -5,22 +5,22 @@ namespace CinemaHikes.Domain.Interfaces;
 public interface IUnitOfWork
 {
     IMovieRepository Movies { get; }
-    
+
     IGenreRepository Genres { get; }
-    
+
     IVideoSourceRepository VideoSources { get; }
-    
+
     IMovieLinkRepository MovieLinks { get; }
-    
+
     ITranslationStudioRepository TranslationStudios { get; }
-    
+
     IReviewRepository Reviews { get; }
-    
+
     IParsingSourceRepository ParsingSources { get; }
-    
+
     IUserMovieRelationRepository<FavoriteMovie> FavoriteMovies { get; }
-    
+
     IUserMovieRelationRepository<ViewHistoryEntry> ViewHistory { get; }
-    
+
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
---
+++ CinemaHikes/src/CinemaHikes.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,39 @@
+using CinemaHikes.Domain.Entities.Users;
+using CinemaHikes.Domain.Interfaces;
+using CinemaHikes.Infrastructure.Persistence.DbContexts;
+
+namespace CinemaHikes.Infrastructure.Repositories;
+
+public sealed class UnitOfWork(
+    CinemaHikesDbContext dbContext,
+    IMovieRepository movies,
+    IGenreRepository genres,
+    IVideoSourceRepository videoSources,
+    IMovieLinkRepository movieLinks,
+    ITranslationStudioRepository translationStudios,
+    IReviewRepository reviews,
+    IParsingSourceRepository parsingSources,
+    IUserMovieRelationRepository<FavoriteMovie> favoriteMovies,
+    IUserMovieRelationRepository<ViewHistoryEntry> viewHistory) : IUnitOfWork
+{
+    public IMovieRepository Movies { get; } = movies;
+
+    public IGenreRepository Genres { get; } = genres;
+
+    public IVideoSourceRepository VideoSources { get; } = videoSources;
+
+    public IMovieLinkRepository MovieLinks { get; } = movieLinks;
+
+    public ITranslationStudioRepository TranslationStudios { get; } = translationStudios;
+
+    public IReviewRepository Reviews { get; } = reviews;
+
+    public IParsingSourceRepository ParsingSources { get; } = parsingSources;
+
+    public IUserMovieRelationRepository<FavoriteMovie> FavoriteMovies { get; } = favoriteMovies;
+
+    public IUserMovieRelationRepository<ViewHistoryEntry> ViewHistory { get; } = viewHistory;
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken) =>
+        await dbContext.SaveChangesAsync(cancellationToken);
+}
