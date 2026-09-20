cd0dbb9934bd0588fd47c07b44b850f9358db5d2
andrvol <v0l0b.and0716@gmail.com>
2026-09-20 21:49:22 +0300

    Added DI (registered repositories interfaces and their relations)

--- CinemaHikes/src/CinemaHikes.Infrastructure/DependencyInjection.cs
+++ CinemaHikes/src/CinemaHikes.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,10 @@
+using CinemaHikes.Domain.Interfaces;
+using CinemaHikes.Domain.Interfaces.Repositories;
 using CinemaHikes.Infrastructure.Persistence.DbContexts;
+using CinemaHikes.Infrastructure.Repositories;
+using CinemaHikes.Infrastructure.Repositories.Catalog;
+using CinemaHikes.Infrastructure.Repositories.Parsing;
+using CinemaHikes.Infrastructure.Repositories.Users;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,7 +19,18 @@ public static class DependencyInjection
                                   throw new InvalidOperationException("No connection  string was found");
 
         services.AddDbContext<CinemaHikesDbContext>(options => options.UseNpgsql());
+        
+        services.AddScoped<IMovieRepository, MovieRepository>();
+        services.AddScoped<IGenreRepository, GenreRepository>();
+        services.AddScoped<IVideoSourceRepository, VideoSourceRepository>();
+        services.AddScoped<IMovieLinkRepository, MovieLinkRepository>();
+        services.AddScoped<ITranslationStudioRepository, TranslationStudioRepository>();
+        services.AddScoped<IReviewRepository, ReviewRepository>();
+        services.AddScoped<IParsingSourceRepository, ParsingSourceRepository>();
+        services.AddScoped(typeof(IUserMovieRelationRepository<>), typeof(UserMovieRelationRepository<>));
 
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
         return services;
     }
 }
